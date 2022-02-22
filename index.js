5f39e9f6ab060095bfd61b6aed304fb33c2343d6
yigitcevk <78323809+yigitcevk@users.noreply.github.com>
2022-02-22 16:28:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.jotform.com/answers/');
-  await page.screenshot({ path: 'example.png' });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.jotform.com/answers/');
+    await page.screenshot({ path: 'example.png' });
 
-  await browser.close();
+    const result = await page.evaluate(() => {
+        let supportPageQuestion = document.querySelectorAll(".dc-f-question");
+        const supportPageQuestionList = [...supportPageQuestion];
+        return supportPageQuestionList.map(h => h.innerText);
+    });
+
+
+    console.log(result);
+
+    await browser.close();
 })();
